e040f02e347a92dc9dc9c9e9a26cec73366a8718
root <root@ip-172-31-40-73.ap-south-1.compute.internal>
2026-03-20 15:24:25 +0000

    new updates

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,7 +1,20 @@
 package com.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("CI/CD Pipeline Working 🚀");
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // loads index.html
     }
 }
